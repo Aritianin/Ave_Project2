e6fbc7e5a778a7082b107ef262ccbb4d27352862
Aritianin <Aritianin@gmail.com>
2020-05-23 13:37:34 +0200

    Kopiowanie linii

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main() {
     cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
+    cout << "Z gory przepraszam za termin" << endl;
     return 0;
 
     // Komentarz Zmiana w
