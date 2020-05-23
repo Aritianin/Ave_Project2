b6f275da37e88b34e70df87d29af72d5b9de32fd
Aritianin <Aritianin@gmail.com>
2020-05-23 15:01:40 +0200

    Tak

---
+++ cmake-build-debug/CMakeFiles/NowyNowyile.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NowyNowyile.dir/main.cpp.obj"
+  "NowyNowyile.exe"
+  "NowyNowyile.exe.manifest"
+  "NowyNowyile.pdb"
+  "libNowyNowyile.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NowyNowyile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NowyNowyile.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NowyNowyile.
+# This may be replaced when dependencies are built.
